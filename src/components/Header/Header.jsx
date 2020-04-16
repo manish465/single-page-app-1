f91cba623f655f4b553d2de6e688ca68c2be4d6b
manish465 <ms5978307@gmail.com>
2020-04-15 21:29:41 -0700

    added logo and login button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from "react";
-import { AppBar, Toolbar } from "@material-ui/core";
+import { AppBar, Toolbar, makeStyles, IconButton } from "@material-ui/core";
+import Logo from "../../assets/LOGO.svg";
+import LoginIcon from "../../assets/LoginButton.svg";
+
+const useStyles = makeStyles({
+    appbar: {
+        background: "#FFFFFF",
+    },
+    logo: {
+        width: 210,
+        height: 90,
+    },
+    loginicon: {
+        width: 77,
+        height: 77,
+    },
+});
 
 const Header = () => {
+    const classes = useStyles();
+
     return (
-        <AppBar>
-            <Toolbar>APPBAR</Toolbar>
+        <AppBar className={classes.appbar}>
+            <Toolbar>
+                <img className={classes.logo} src={Logo} alt='' />
+                <IconButton>
+                    <img className={classes.loginicon} src={LoginIcon} alt='' />
+                </IconButton>
+            </Toolbar>
         </AppBar>
     );
 };
