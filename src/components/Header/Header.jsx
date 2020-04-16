58891ba01c8bbb2a336a4baa873a0773ffaba30d
manish465 <ms5978307@gmail.com>
2020-04-15 22:25:47 -0700

    added tabs

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,8 @@ import {
     makeStyles,
     IconButton,
     Grid,
+    Button,
+    Typography,
 } from "@material-ui/core";
 import Logo from "../../assets/LOGO.svg";
 import LoginIcon from "../../assets/LoginButton.svg";
@@ -22,6 +24,12 @@ const useStyles = makeStyles({
         width: 77,
         height: 77,
         flexGrow: 1,
+        color: "#292929",
+    },
+    tabs: {
+        color: "#414141",
+        fontSize: "54px",
+        fontFamily: "Letter Gothic Std",
     },
 });
 
@@ -29,13 +37,41 @@ const Header = () => {
     const classes = useStyles();
 
     return (
-        <AppBar className={classes.appbar}>
+        <AppBar className={classes.appbar} position='static'>
             <Toolbar>
                 <Grid container justify='space-between'>
-                    <Grid item>
+                    <Grid xs={2} item>
                         <img className={classes.logo} src={Logo} alt='' />
                     </Grid>
-                    <Grid item>
+                    <Grid
+                        xs={6}
+                        alignContent='center'
+                        justify='center'
+                        spacing={10}
+                        container>
+                        <Grid item>
+                            <Button>
+                                <Typography className={classes.tabs}>
+                                    HOME
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button>
+                                <Typography className={classes.tabs}>
+                                    SHOP
+                                </Typography>
+                            </Button>
+                        </Grid>
+                        <Grid item>
+                            <Button>
+                                <Typography className={classes.tabs}>
+                                    BLOG
+                                </Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid xs={2} item>
                         <IconButton>
                             <img
                                 className={classes.loginicon}
